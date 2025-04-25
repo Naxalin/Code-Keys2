ecf438d0325625e90e1fb6e327f6f6f4d0aced8e
Naxalin <gonzialconerofdez@gmail.com>
2025-04-25 13:45:03 +0200

    feat: add Tailwind CSS integration and update components
    - Added Tailwind CSS as a dev dependency in package.json.
    - Created input.css and output.css for Tailwind CSS styles.
    - Updated HelloWorld.vue to include Tailwind CSS classes.
    - Modified WelcomeItem.vue to apply Tailwind CSS classes to slot.
    - Added tailwind.config.js for Tailwind CSS configuration.

--- src/components/WelcomeItem.vue
+++ src/components/WelcomeItem.vue
@@ -5,7 +5,7 @@
     </i>
     <div class="details">
       <h3>
-        <slot name="heading"></slot>
+        <slot class="flex block" name="heading"></slot>
       </h3>
       <slot></slot>
     </div>
