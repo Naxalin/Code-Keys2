4a8e82255b03f9aca20838bf0a304b654c725aca
Naxalin <gonzialconerofdez@gmail.com>
2025-05-07 21:31:57 +0200

    feat: add initial project structure with routing and views

---
+++ src/views/InicioView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Main from '@/components/Main.vue';
+import DefaultLayout from '@/layouts/DefaultLayout.vue';
+</script>
+
+<template>
+    <DefaultLayout>
+        <Main />
+    </DefaultLayout>
+</template>
