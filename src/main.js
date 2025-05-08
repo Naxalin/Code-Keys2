66c2111cc169e49f105309aa3946073496f55b40
Naxalin <gonzialconerofdez@gmail.com>
2025-05-08 08:35:09 +0200

    feat: add images and update routing for new views including Sesion, Detalles, Registro, DerechosAutor, Cookies, and PoliticasPrivacidad

--- src/main.js
+++ src/main.js
@@ -2,5 +2,12 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import DefaultLayout from './layouts/DefaultLayout.vue'
+import 'flowbite'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+app.component('DefaultLayout', DefaultLayout)
+app.mount('#app')
